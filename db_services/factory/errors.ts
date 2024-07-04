4826b499820aa804121eaec05bb916b35f9249cd
Agnese Bruglia <brugliaagnese@gmail.com>
2024-07-04 13:01:20 +0200

    Aggiornamento middleware

--- db_services/factory/errors.ts
+++ db_services/factory/errors.ts
@@ -15,14 +15,54 @@ interface IErrorObj {
 export class ZeroTokensAvailable implements IErrorObj {
     getErrorObj(): { message: string; status: number } {
         return { 
-            message: Message.zeroTokensAvailable_messagge,
-            status: 400
+            status: 401,
+            message: Message.zeroTokensAvailable_messagge
+        }
+    }
+}
+
+export class NotEnoughTokens implements IErrorObj {
+    getErrorObj(): { message: string; status: number } {
+        return { 
+            status: 401,
+            message: Message.notEnoughTokens_message
+        }
+    }
+}
+
+export class UserDoesNotExist implements IErrorObj {
+    getErrorObj(): { message: string; status: number } {
+        return { 
+            status: 400,
+            message: Message.userDoesNotExist_message
+        }
+    }
+}
+
+export class UserAlreadyExists implements IErrorObj {
+    getErrorObj(): { message: string; status: number } {
+        return { 
+            status: 400,
+            message: Message.userAlreadyExists_message
+        }
+    }
+}
+
+export class UserNotAdmin implements IErrorObj {
+    getErrorObj(): { message: string; status: number } {
+        return { 
+            status: 404,
+            message: Message.userNotAdmin_message
         }
     }
 }
 
 export enum EnumError {
     ZeroTokensAvailable,
+    NotEnoughTokens,
+    UserDoesNotExist,
+    UserAlreadyExists,
+    UserNotAdmin
 }
 
 export function getError(type: EnumError): IErrorObj {
@@ -30,6 +70,18 @@ export function getError(type: EnumError): IErrorObj {
     switch (type){
         case EnumError.ZeroTokensAvailable:
             val = new ZeroTokensAvailable();
+            break;
+        case EnumError.NotEnoughTokens:
+            val = new NotEnoughTokens();
+            break;
+        case EnumError.UserDoesNotExist:
+            val = new UserDoesNotExist();
+            break;
+        case EnumError.UserAlreadyExists:
+            val = new UserAlreadyExists();
+            break;
+        case EnumError.UserNotAdmin:
+            val = new UserNotAdmin();
             break;
     }
     return val; 
