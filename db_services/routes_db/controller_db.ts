4d31c387ee971b213267f92def381fd9db26d6ed
lucabellantee <s1097955@studenti.univpm.it>
2024-07-04 14:55:20 +0200

    Modificato controller db ed aggiunto nuove funzionanilità

--- db_services/routes_db/controller_db.ts
+++ db_services/routes_db/controller_db.ts
@@ -11,6 +11,13 @@ interface UserInput {
     residual_tokens: number;
 }
 
+interface Json {
+    successo: boolean;
+    data?: any; 
+    errore?: string;
+}
+
+
 /*
 *
 * Funzione per l'inserimento di un nuovo utente nel DB
@@ -49,7 +56,7 @@ export async function addDataset(dataset_name: string, id_user: number): Promise
         // Creazione della tupla nel database
         await Dataset.create({
             dataset_name: dataset_name,
-            UserId: id_user  // Associa il dataset all'utente tramite la relazione definita
+            id: id_user  // Associa il dataset all'utente tramite la relazione definita
         });
     // Todo LUCA: vedere di fare i controlli per assicurarsi che non esiste uno stesso user con uno stesso dataset
         return dataset_name;
@@ -73,9 +80,9 @@ export async function getAllUsers(): Promise<any[]> {
 
 
 /**
- * Funzione per aggiungere un nuovo dataset e creare la relativa cartella.
+ * Funzione per ritornare dataset.
  * @param id_user ID dell'utente associato al dataset.
- * @param dataset_name Nome del dataset (chiave primaria). Parametro facoltativo
+ * @param dataset_name Nome del dataset (chiave primaria). Parametro facoltativo.
  * @returns Dataset specifico riferito ad uno utente oppure tutti i dataset di un'utente. Ritorna un eccezione in caso di errore
  */
 export async function getDatasets(id_user:String, dataset_name?: String) {
@@ -86,11 +93,11 @@ export async function getDatasets(id_user:String, dataset_name?: String) {
             const result = await Dataset.findAll({
                 where: {
                     dataset_name : dataset_name,
-                    id_user : id_user,
+                    id : id_user,
                 },
             });
             return {
-                success: true,
+                successo: true,
                 data: result 
             }; 
         }
@@ -98,35 +105,120 @@ export async function getDatasets(id_user:String, dataset_name?: String) {
         else{
             const result = await Dataset.findAll({
                 where: {
-                    id_user : id_user,
+                    id : id_user,
                 },
             });
             return {
-                success: true,
+                successo: true,
                 data: result 
             }; 
         }
 
     } catch (error:any) {
         return {
-            success: false,
-            message: error.message
+            successo: false,
+            errore: error.message
         };
     }
 }
 
-export async function getAllDataset(){
+
+
+/**
+ * Funzione per ritornare tutti i dataset presenti nel sistema.
+ * @returns Tutti i dataset presenti nel DB.
+ */
+export async function getAllDataset(): Promise<Json>{
     try{
         const result = await Dataset.findAll();
         return {
-            success: true,
+            successo: true,
             data: result
         }
     }
     catch(error:any){
         return{
-            success: false,
-            message: error.message
+            successo: false,
+            errore: error.message
+        }
+    }
+}
+
+
+
+/**
+ * Funzione per ritornare dataset.
+ * @param id_user ID dell'utente associato al dataset.
+ * @param dataset_name Nome del dataset (chiave primaria).
+ * @returns Messaggio di buona riuscita oppure messaggio di errore in forma Json.
+ */
+export async function updateDataset(id_user:String, dataset_name: String, new_dataset_name: String): Promise<Json>{
+    try{
+        await Dataset.update(
+            { dataset_name: new_dataset_name},
+            {
+              where: {
+                id: id_user,
+                dataset_name: dataset_name
+              },
+            },
+          );
+        
+        return {
+            successo: true,
+            data: "Modifica del dataset avvenuta correttamente."
+        };
+    }
+    catch(error: any){
+        return {
+            successo: false,
+            data: error.message
+        };
+    }
+}
+
+
+/**
+ * Funzione per aggiungere nuovi video al dataset dello user.
+ * @param id_user ID dell'utente associato al dataset.
+ * @param dataset_name Nome del dataset (chiave primaria).
+ * @param new_videos Nuovi video da aggiungere al dataset.
+ * @returns Messaggio di buona riuscita oppure messaggio di errore in forma Json.
+ */
+export async function insertVideoIntoDataset(id_user: String, dataset_name: String, new_videos: Array<String>): Promise<Json>{
+    try{
+        const videos = await Dataset.findOne({
+            where: {
+                id : id_user,
+                dataset_name: dataset_name,
+            },
+            attributes: ['videos']
+        }); 
+
+        if(videos != null){
+            const old_videos: Array<String> = videos.getDataValue('videos');
+            const videos_path_complete: Array<String> = new_videos.map(fileName => '/app/dataset_&_modelli/dataset/' + fileName);
+            const video: Array<String> = old_videos.concat(videos_path_complete);
+            await Dataset.update(
+                { videos: video},
+                {
+                  where: {
+                    id: id_user,
+                    dataset_name: dataset_name
+                  },
+                },
+              );
+        }
+        return {
+            successo: true,
+            data: 'I video sono stati correttamente inseriti nel dataset'
+        }
+        
+    }
+    catch(error:any){
+        return {
+            successo: false,
+            data: error.message
         }
     }
 }
--- db_services/server.ts
+++ db_services/server.ts
@@ -1,6 +1,8 @@
 const express = require('express'); 
 import { Request, Response } from 'express';
-const { createUser, addDataset, getAllUsers, getDatasets, getAllDataset } = require('./routes_db/controller_db'); 
+const { createUser, addDataset, getAllUsers, getDatasets, getAllDataset,
+    updateDataset, insertVideoIntoDataset
+} = require('./routes_db/controller_db'); 
 
 const app = express();
 const port = 3000;
@@ -55,12 +57,31 @@ app.get('/getDataset/:id_user/:dataset_name', async (req: Request, res: Response
 
 
 // Definizione della rotta per recuperare tutti gli utenti
-app.get('/prova', async (req: Request, res: Response) => {
+app.get('/prova/:id_user/:dataset_name/:new_dataset_name', async (req: Request, res: Response) => {
+    const result = await updateDataset(req.params.id_user, req.params.dataset_name, req.params.new_dataset_name);
+    return res.json(result);
+});
+app.get('/getDataset', async (req: Request, res: Response) => {
     const result = await getAllDataset()
     return res.json(result);
 });
 
+app.put('/dataset/:id_user/:dataset_name', async (req: Request, res: Response) => {
+    const { id_user, dataset_name } = req.params;
+    const { new_videos } = req.body;
 
+    try {
+        const result = await insertVideoIntoDataset(id_user, dataset_name, new_videos);
+
+        if (result.successo) {
+            res.status(200).json(result);
+        } else {
+            res.status(500).json(result);
+        }
+    } catch (error:any) {
+        res.status(500).json({ successo: false, data: error.message });
+    }
+});
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
